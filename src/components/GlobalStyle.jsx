1d27033d188a334a46aaad6cdab058e7faa39452
chihoonahn0319 <clgnseowkd@naver.com>
2023-07-11 00:21:45 +0900

    "반응형css추가"

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
 
-//styled-components 라이브러리에서 제공하는 함수
-//전역적으로 적용되는 스타일을 생성할 때 사용
-
 const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
   }
-  //버튼의 기본 값 
+
   button {
     width: 140px;
     height: 40px;
@@ -18,14 +15,18 @@ const GlobalStyle = createGlobalStyle`
     border-radius: 40px;
   }
 
-    //페이지 여백 설정
-    body {
+  body {
     margin: 0;
   }
 
+  @media (max-width: 768px) {
+    /* 페이지 너비가 768px 이하일 때 적용되는 스타일 */
+    button {
+      width: 100px;
+      height: 30px;
+      font-size: 14px;
+    }
+  }
 `;
 
 export default GlobalStyle;
-//GlobalStyle 컴포넌트를 외부로 내보냄
-// 이를 통해 다른 파일에서 GlobalStyle 컴포넌트를
-//전역 스타일을 적용할 수 있습
